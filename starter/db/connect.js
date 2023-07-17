d9e8eb353b612559a45178e4f72fc50474a2ee70
chaieeetanya433 <129309578+chaieeetanya433@users.noreply.github.com>
2023-07-17 20:13:52 +0530

    Update connect.js

--- starter/db/connect.js
+++ starter/db/connect.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose');   //importing mongoose
 
-// const connectionString = 
-// 'mongodb+srv://chai_eee_tanya:619@cluster0.jjdeb4h.mongodb.net/01-TASK-MANAGER?retryWrites=true&w=majority'
-
 const connectDB = (url) => {
     return mongoose.connect(url, {    
         useNewUrlParser: true, 
